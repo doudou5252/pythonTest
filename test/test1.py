090a02bede71052e8067536e4a3555e3ac84e1f4
doudou1212 <1358818004@qq.com>
2019-01-14 16:17:46 +0800

    test-commit

---
+++ test/test1.py
@@ -0,0 +1,14 @@
+# 关键字
+import keyword
+print(keyword.kwlist)
+if True:
+    print('True')
+else:
+    print("false")
+    print('duode')
+
+input("\n\n 按下 enter 键后退出。")
+
+
+
+
---
+++ test/test2array.py
@@ -0,0 +1,29 @@
+# name_list = ["doudou11", "doudu222", "doudou"]
+#
+# print(name_list[1])
+# print(name_list.index('doudou'))
+# name_list[1] = "李四"
+# print(name_list[1])
+# name_list.append("网校问")
+# name_list.insert(1, "添加")
+# print(name_list)
+#
+#
+# temp_list = ["我是1", "我是2", "我是三"]
+# name_list.extend(temp_list)
+# print(name_list)
+#
+# name_list.remove("我是三")
+#
+# name_list.pop()
+#
+# name_list.pop(3)
+#
+# name_list.clear()
+# print(name_list)
+
+
+# del测试
+
+
+
